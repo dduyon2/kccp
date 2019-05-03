ceee577d63d8908de15f9784eb14e03541dada21
SooYeonKim <ksy9500@naver.com>
2019-05-03 13:43:40 +0900

    Map

---
+++ app/src/main/java/com/example/homescreen/MainActivity.java
@@ -0,0 +1,305 @@
+package com.example.homescreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import com.example.homescreen.ObjectClickImageView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ObjectClickImageView objectView = (ObjectClickImageView)findViewById(R.id.dobong_image);
+        ObjectClickImageView objectView1 = (ObjectClickImageView)findViewById(R.id.nowon_image);
+        ObjectClickImageView objectView2 = (ObjectClickImageView)findViewById(R.id.dongdaemoon_image);
+        ObjectClickImageView objectView3 = (ObjectClickImageView)findViewById(R.id.enpyeong_image);
+        ObjectClickImageView objectView4 = (ObjectClickImageView)findViewById(R.id.joong_image);
+        ObjectClickImageView objectView5 = (ObjectClickImageView)findViewById(R.id.jongro_image);
+        ObjectClickImageView objectView6 = (ObjectClickImageView)findViewById(R.id.seodaemoon_image);
+        ObjectClickImageView objectView7 = (ObjectClickImageView)findViewById(R.id.seongbook_image);
+        ObjectClickImageView objectView8 = (ObjectClickImageView)findViewById(R.id.seongdong_image);
+        ObjectClickImageView objectView9 = (ObjectClickImageView)findViewById(R.id.guangjin_image);
+        ObjectClickImageView objectView10 = (ObjectClickImageView)findViewById(R.id.gangnam_image);
+        ObjectClickImageView objectView11 = (ObjectClickImageView)findViewById(R.id.gangbook_image);
+        ObjectClickImageView objectView12 = (ObjectClickImageView)findViewById(R.id.gangdong_image);
+        ObjectClickImageView objectView13 = (ObjectClickImageView)findViewById(R.id.gangseo_image);
+        ObjectClickImageView objectView14 = (ObjectClickImageView)findViewById(R.id.guanak_image);
+        ObjectClickImageView objectView15 = (ObjectClickImageView)findViewById(R.id.guro_image);
+        ObjectClickImageView objectView16 = (ObjectClickImageView)findViewById(R.id.geumcheon_image);
+        ObjectClickImageView objectView17 = (ObjectClickImageView)findViewById(R.id.dongjak_image);
+        ObjectClickImageView objectView18 = (ObjectClickImageView)findViewById(R.id.joonglang_image);
+        ObjectClickImageView objectView19 = (ObjectClickImageView)findViewById(R.id.mapo_image);
+        ObjectClickImageView objectView20 = (ObjectClickImageView)findViewById(R.id.seocho_image);
+        ObjectClickImageView objectView21 = (ObjectClickImageView)findViewById(R.id.songpa_image);
+        ObjectClickImageView objectView22 = (ObjectClickImageView)findViewById(R.id.yangcheon_image);
+        ObjectClickImageView objectView23 = (ObjectClickImageView)findViewById(R.id.yeongdeungpo_image);
+        ObjectClickImageView objectView24 = (ObjectClickImageView)findViewById(R.id.yongsan_image);
+
+        objectView.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Dobong.class);
+                startActivity(intent);
+            }
+        });
+        objectView1.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Nowon.class);
+                startActivity(intent);
+            }
+        });
+        objectView2.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Dongdaemoon.class);
+                startActivity(intent);
+            }
+        });
+        objectView3.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Enpyeong.class);
+                startActivity(intent);
+            }
+        });
+        objectView4.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Joong.class);
+                startActivity(intent);
+            }
+        });
+
+        objectView5.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Jongro.class);
+                startActivity(intent);
+            }
+        });
+        objectView6.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Seodaemoon.class);
+                startActivity(intent);
+            }
+        });
+        objectView7.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Seongbook.class);
+                startActivity(intent);
+            }
+        });
+        objectView8.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Seongdong.class);
+                startActivity(intent);
+            }
+        });
+        objectView9.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Guangjin.class);
+                startActivity(intent);
+            }
+        });
+        objectView10.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Gangnam.class);
+                startActivity(intent);
+            }
+        });
+        objectView11.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Gangbook.class);
+                startActivity(intent);
+            }
+        });
+        objectView12.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Gangdong.class);
+                startActivity(intent);
+            }
+        });
+        objectView13.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Gangseo.class);
+                startActivity(intent);
+            }
+        });
+        objectView14.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Guanak.class);
+                startActivity(intent);
+            }
+        });
+        objectView15.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Guro.class);
+                startActivity(intent);
+            }
+        });
+        objectView16.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Geumcheon.class);
+                startActivity(intent);
+            }
+        });
+        objectView17.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Dongjak.class);
+                startActivity(intent);
+            }
+        });
+        objectView18.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Joonglang.class);
+                startActivity(intent);
+            }
+        });
+        objectView19.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Mapo.class);
+                startActivity(intent);
+            }
+        });
+        objectView20.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Seocho.class);
+                startActivity(intent);
+            }
+        });
+        objectView21.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Songpa.class);
+                startActivity(intent);
+            }
+        });
+        objectView22.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Yangcheon.class);
+                startActivity(intent);
+            }
+        });
+        objectView23.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Yeongdeungpo.class);
+                startActivity(intent);
+            }
+        });
+        objectView24.setOnObjectClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Yongsan.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+}
---
+++ app/src/main/java/com/example/homescreen/Yeongdeungpo.java
@@ -0,0 +1,25 @@
+package com.example.homescreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Yeongdeungpo extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.yeongdeungpo);
+
+        Button b1 = (Button) findViewById(R.id.back_button);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent2);
+            }
+        });
+    }
+}
